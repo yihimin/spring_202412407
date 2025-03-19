8ebc443a03194c2682a8000075c68be10144f3c4
yihimin <yihimin01@gmail.com>
2025-03-19 16:54:13 +0900

    2주차

--- src/main/java/idusw/sbb/b202412407/controller/HIRestController.java
+++ src/main/java/idusw/sbb/b202412407/controller/HIRestController.java
@@ -1,9 +1,10 @@
 package idusw.sbb.b202412407.controller;
 
 import idusw.sbb.b202412407.B202412407Application;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -14,5 +15,12 @@ public class HIRestController {
     @GetMapping("/hi") //HTTP 메소드 중 Get 방식의 요청 / hi와 하단의 메소드 sayHi()리턴
     public String sayHi() {
         return "<h1> hihi </h1>"; // HTTP Response Body, 직접 응답
+    }
+
+    @PostMapping("/hi") // 🔥 POST 요청 처리
+    public ResponseEntity<String> sayHihi(HttpServletRequest request) {
+        String name = request.getParameter("p-name"); // 폼 데이터에서 name="p-name"을 가져옴
+        String email = request.getParameter("p-email"); // 폼 데이터에서 name="p-email"을 가져옴
+        return ResponseEntity.ok("<h1> 받았다!: " + name + " - " + email + " - " + request.getParameter("p-phone") +" - "+ request.getParameter("p-message")+ " </h1>");
     }
 }
