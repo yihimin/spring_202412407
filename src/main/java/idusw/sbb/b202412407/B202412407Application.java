8fe6a3987e2749e24c3cf6bc09374449a94663ed
yihimin <yihimin01@gmail.com>
2025-03-19 14:22:48 +0900

    Initial commit

---
+++ src/main/java/idusw/sbb/b202412407/B202412407Application.java
@@ -0,0 +1,13 @@
+package idusw.sbb.b202412407;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B202412407Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B202412407Application.class, args);
+    }
+
+}
---
+++ src/test/java/idusw/sbb/b202412407/B202412407ApplicationTests.java
@@ -0,0 +1,13 @@
+package idusw.sbb.b202412407;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B202412407ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
